0a6712d5f6ba26f1dcd68eab511f76656612c4d8
Diana Ambarwati Febriani <dianaambar745@gmail.com>
2020-06-21 14:05:53 +0700

    penerima donasi non relawan

--- components/CardReportNonRelawan.vue
+++ components/CardReportNonRelawan.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="card-report">
-    <v-row>
+    <v-row v-if="datadonasi.status == 'Donasi diterima, Mencari Relawan'">
       <v-col cols="4">
         <!--<tr>-->
         <p>Nama Penerima :</p>
         <!--</tr>-->
         <!--<tr>-->
         <p>Alamat Penerima :</p>
+        <p>Foto :</p>
+        <p>Latitude :</p>
+        <p>Longitude :</p>
         <!--</tr>-->
       </v-col>
       <v-col cols="8">
         <div v-if="editing">
           <v-text-field v-model="namaValue" dense label="Nama Penerima"></v-text-field>
           <v-text-field v-model="alamatValue" dense label="Alamat"></v-text-field>
+          <v-text-field v-model="foto" dense label="foto"></v-text-field>
+          <v-text-field v-model="latitude" dense label="Latitude"></v-text-field>
+          <v-text-field v-model="longitude" dense label="Longitude"></v-text-field>
           <v-btn @click="saveEdit" color="grey darken-1">Simpan</v-btn>
         </div>
         <div v-else>
           <p>{{form.nama_penerima}}</p>
           <p>{{form.alamat_penerima}}</p>
+          <p>{{form.foto}}</p>
+          <p>{{form.latitude}}</p>
+          <p>{{form.longitude}}</p>
           <v-btn @click="isEdit" color="grey darken-1">Edit</v-btn>
         </div>
-        <v-btn class="btn-acc" color="green darken-3">Kirim Laporan</v-btn>
+        <v-btn @click="updatePenerima" class="btn-acc" color="green darken-3">Kirim Laporan</v-btn>
       </v-col>
     </v-row>
     <div>
@@ -39,13 +48,28 @@ export default {
       editing: true,
       namaValue: null,
       alamatValue: null,
+      foto: null,
+      show: false,
+      latitude: null,
+      longitude: null,
       form: {
         nama_penerima: "",
-        alamat_penerima: ""
+        alamat_penerima: "",
+        foto: "",
+        latitude: "",
+        longitude: ""
       }
     };
   },
   methods: {
+    async updatePenerima() {
+      await this.$axios.$post(
+        "updatepenerima/" + this.datadonasi.id,
+        this.form
+      );
+      this.show = false;
+      window.location.reload(true);
+    },
     isEdit: function() {
       this.editing = true;
     },
@@ -56,6 +80,9 @@ export default {
       (this.editing = false),
         (this.form.nama_penerima = this.namaValue),
         (this.form.alamat_penerima = this.alamatValue);
+      this.form.foto = this.foto;
+      this.form.latitude = this.latitude;
+      this.form.longitude = this.longitude;
     }
   }
 };
